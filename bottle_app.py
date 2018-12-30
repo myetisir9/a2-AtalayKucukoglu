0e71193f687542e9d82cb60d84adeda25b6e5d0e
AtalayKucukoglu <34166678+AtalayKucukoglu@users.noreply.github.com>
2018-12-30 15:54:57 +0300

    static file added & login added

--- bottle_app.py
+++ bottle_app.py
@@ -4,53 +4,31 @@
 ### You can alter the below code to make your own dynamic website.
 ### The landing page for assignment 3 should be at /
 #####################################################################
-from typing import List, Any
-from bottle import route, run, default_app, debug, static_file, template, request
+import codecs
+from bottle import *
 from hashlib import sha256
-from repl_comments import create_hash, correct_password
 
+## retrived from
+# https://bitbucket.org/damienjadeduff/hashing_example/raw/master/hash_password.py
 
-def htmlify(title,text):
-    page = """
-        <!doctype html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8" />
-                <title>%s</title>
-            </head>
-            <body>
-            %s
-            </body>
-        </html>
+## password: simplicity
+correct_password = "2ae0514924b1db04e8c45002326641b5e6c74f182288ae8702d5c7250374669e"
+comments = []
+logged_in = False
 
-    """ % (title,text)
-    return page
-
-
-
-
-def index():
-    return htmlify("My lovely website",
-                   "This is going to be an awesome website, when it is finished.")
-route('/', 'GET', index)
+def create_hash(password):
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
 
 def static_file_callback(filename):
     return static_file(filename, root='static')
 
 route('/static/<filename>', 'GET', static_file_callback)
 
-@route('/login')
+@route('/')
 
 def login_page():
-    return '''
-        <h1>Welcome</h1>
-        <h3>You have to login first to add comments.</h3>
-        <form action="/login" method="post">
-            Username: <input name="username" type="text" />
-            Password: <input name="password" type="password" />
-            <input value="Login" type="submit" />
-        </form>
-    '''
+    return template('./static/index.html')
 
 @route('/login', method='POST')
 
@@ -61,14 +39,81 @@ def do_login():
         return "<h1>Welcome {}!</h1>" \
                "<h2>Your login information was correct.</h2>".format(username)
     else:
-        return "<h1>Login failed.</h1>"
+        return '''
+        <h1>Login Failed</h1>
+        <button onclick="location.href='{}'">Try Again</button>
+
+    '''
 
 def check_login(username, password):
     input_password = create_hash(password)
     if input_password == correct_password:
+        logged_in = True
         return True
     else:
         return False
+
+def add_comments():
+    i = 0
+    while True:
+        print("Enter comment (or exit with -1): ")
+        comment = input()
+        if comment == "-1":
+            print("Exitted succesfully")
+            break
+        else:
+            comments.append(comment)
+            print("Comments:")
+            for i in range(len(comments)):
+                print(str(i + 1) + ". " + comments[i])
+
+            i += 1
+
+
+
+#route to css stylesheet
+
+@get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='static/css')
+
+# route to the pages
+
+@route('/index.html')
+def index():
+    return template('./static/index.html')
+
+@route('/syrup.html')
+def syrup():
+    return template('./static/syrup.html')
+
+@route('/milk.html')
+def milk():
+    return template('./static/milk.html')
+
+@route('/baklava.html')
+def baklava():
+    return template('./static/baklava.html')
+
+@route('/kunefe.html')
+def kunefe():
+    return template('./static/kunefe.html')
+
+@route('/kadayif.html')
+def kadayif():
+    return template('./static/kadayif.html')
+
+@route('/keskul.html')
+def keskul():
+    return template('./static/keskul.html')
+
+@route('/sutlac.html')
+def sutlac():
+    return template('./static/sutlac.html')
+
+@route('/kazandibi.html')
+def kazandibi():
+    return template('./static/kazandibi.html')
 
 
 
