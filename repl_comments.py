4d7a40acd4ce4863f837ce13d41f5a316f243231
AtalayKucukoglu <34166678+AtalayKucukoglu@users.noreply.github.com>
2018-12-30 23:23:56 +0300

    repl_comments fixed

--- repl_comments.py
+++ repl_comments.py
@@ -1,12 +1,11 @@
 
 from hashlib import sha256
-from typing import List, Any
 
 
 ## derived from
 # https://bitbucket.org/damienjadeduff/hashing_example/raw/master/hash_password.py
 
-comments = {}
+comments = []
 ## password: simplicity
 correct_password = "2ae0514924b1db04e8c45002326641b5e6c74f182288ae8702d5c7250374669e"
 
@@ -24,7 +23,7 @@ def add_comments():
             print("Exitted succesfully")
             break
         else:
-            comments.push(comment)
+            comments.append(comment)
             print("Comments:")
             for i in range(len(comments)):
                 print(str(i + 1) + ". " + comments[i])
@@ -36,13 +35,14 @@ def main():
         print("Please enter your password, so that we can understand it is YOU.")
         input_password = create_hash(input())
 
-        if input_password == password:
+        if input_password == correct_password:
             print("Access granted.")
             break
         else:
             print("Incorrect password. Access denied.")
-            return
+            continue
 
     add_comments()
 
+main()
 
